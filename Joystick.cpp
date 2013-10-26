997df2418b8a7cc5fce613799bde560ccad70a25
niharmitra <nihar.roy.mitra@gmail.com>
2013-10-26 14:54:39 -0700

    Update Joystick.cpp
    Initializes stuff

--- Joystick.cpp
+++ Joystick.cpp
@@ -9,4 +9,9 @@ Autonomous: Rotates 90 degrees left, then right, like calibration
 
 class MyRobot : public IterativeRobot {
 
+Victor leftVic1;
+Victor leftVic2;
+Victor rightVic1;
+Victor rightVic2;
+Timer t;
 }
