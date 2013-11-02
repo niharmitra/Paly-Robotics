fec7f8bc2930c5324096e31371425c9978f2c181
niharmitra <nihar.roy.mitra@gmail.com>
2013-11-02 14:38:32 -0700

    Trimmed down the non-shooter code

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,18 +1,14 @@
-#include "WPIlib.h"
-#define PORT_DRIVE_VIC_1 5 //macro substitutes first part with second whenever first is used
-#define PORT_DRIVE_VIC_2 10
-#define PORT_DRIVE_VIC_3 2
-#define PORT_DRIVE_VIC_4 1
-#define PORT_JS_SPEED 1
-#define PORT_JS_TURN 2
-#define PORT_JS_OPERATOR 3
-#define PORT_SHOOTER_VIC_1 3
-#define PORT_SHOOTER_VIC_2 4
-#define SHOOT_SPEED 0.5
-#define PORT_COMPRESSOR 1
-#define SPIN_WAIT 5000
+//Code for the shoooter. Trimmed down to only ahve the stuff for the shooter.
 
-//sets up
+#include "WPIlib.h"
+#define PORT_JS_OPERATOR 3 //port for the operator joystick
+#define PORT_SHOOTER_VIC_1 3 //port for one shooter motor
+#define PORT_SHOOTER_VIC_2 4 //port for the other shooter motor
+#define SHOOT_SPEED 0.5 //speed for shooter motors
+#define PORT_COMPRESSOR 1 //port for the compressor
+#define SPIN_WAIT 5000 //how long to wait while spinning up to speed
+
+//labels the states with more useable names
 enum {
   IDLE,
   SPIN_UP,
@@ -22,29 +18,17 @@ enum {
 
 class MyRobot : public IterativeRobot {
 
-Victor leftVic1;
-Victor leftVic2;
-Victor rightVic1;
-Victor rightVic2;
-Timer t;
-Joystick speedStick;
-Joystick turnStick;
 Joystick operatorStick;
 Victor 
 
-
+//initialization list
 public:
 MyRobot() :
-  leftVic1(PORT_DRIVE_VIC_1),
-  leftVic2(PORT_DRIVE_VIC_2), //initialization list (saves memory by using this format)
-  rightVic1(PORT_DRIVE_VIC_3),
-  rightVic2(PORT_DRIVE_VIC_4),
-  speedStick(PORT_JS_SPEED),
-  turnStick(PORT_JS_TURN),
   operatorStick(PORT_JS_OPERATOR)
 {
 
 }
+//sets up the different iterative robot methods
   void AutonomousInit();
   void AutonomousPeriodic();
   void AutonomousDisabled();
@@ -100,8 +84,5 @@ void MyRobot::TeleopPeriodic()
 
 void MyRobot::TeleopDisabled() 
 {
-  leftVic1.Set(0);
-  leftVic2.Set(0);
-  rightVic1.Set(0);
-  rightVic2.Set(0);
+  
 }
