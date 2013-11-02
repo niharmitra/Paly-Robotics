763bd74ab0b7df7e8fd19552bc92df752f936333
niharmitra <nihar.roy.mitra@gmail.com>
2013-11-02 15:14:07 -0700

    Set up compressor, solenoid, victors

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,12 +1,15 @@
-//Code for the shoooter. Trimmed down to only ahve the stuff for the shooter.
+//Code for the shoooter. Trimmed down to only have the stuff for the shooter.
 
 #include "WPIlib.h"
 #define PORT_JS_OPERATOR 3 //port for the operator joystick
 #define PORT_SHOOTER_VIC_1 3 //port for one shooter motor
 #define PORT_SHOOTER_VIC_2 4 //port for the other shooter motor
-#define SHOOT_SPEED 0.5 //speed for shooter motors
-#define PORT_COMPRESSOR 1 //port for the compressor
+#define PORT_PISTON
+#define PORT_COMPRESSOR_RELAY 1 //port for the compressor relay
+#define PORT_COMPRESSOR_CUTOFF 1 //port for the compressor cutoff
 #define SPIN_WAIT 5000 //how long to wait while spinning up to speed
+#define SHOOT_SPEED 0.5 //speed for shooter motors
+
 
 //labels the states with more useable names
 enum {
@@ -19,12 +22,17 @@ enum {
 class MyRobot : public IterativeRobot {
 
 Joystick operatorStick;
-Victor 
+Victor shootVic1;
+Victor shootVic2;
+Solenoid shooterSol;
 
-//initialization list
+//initialization list (constructor)
 public:
 MyRobot() :
-  operatorStick(PORT_JS_OPERATOR)
+  operatorStick(PORT_JS_OPERATOR),
+  shootVic1(PORT_SHOOTER_VIC_1),
+  shootVic2(PORT_SHOOTER_VIC_2)
+  
 {
 
 }
@@ -35,7 +43,15 @@ MyRobot() :
   void TeleopInit();
   void TeleopPeriodic();
   void TeleopDisabled();
+//sets up robot init method
+  void RobotInit();
 };
+
+void MyRobot::RobotInit() 
+{
+  Compressor *c = new Compressor(PORT_COMPRESSOR_CUTOFF, PORT_COMPRESSOR_RELAY);
+  c->Start();
+}
 
 void MyRobot::AutonomousInit()
 {
@@ -59,24 +75,29 @@ void MyRobot::TeleopInit()
 
 void MyRobot::TeleopPeriodic()
 {
-  if(shooterState == IDLE){
+  if(shooterState == IDLE)
+  {
     //if trigger pressed, change to SPIN_UP
-    if(operatorStick.GetTrigger()){
+    if(operatorStick.GetTrigger())
+    {
       shooterState = SPIN_UP
     }
   }
   
-  else if(shooterState == SPIN_UP){
+  else if(shooterState == SPIN_UP)
+  {
     //if max speed reached, change to EXTENDING
      
   }
   
-  else if(shooterState == EXTENDING){
+  else if(shooterState == EXTENDING)
+  {
     //if frisbee shot, change to RECEDING
     
   }
   
-  else if(shooterState == RECEDING){
+  else if(shooterState == RECEDING)
+  {
     //if piston receeded, change to IDLE
     
   }
